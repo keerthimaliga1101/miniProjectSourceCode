14b6e9db711195aae6476a71d985c428928588f4
Keerthimaliga S <keerthimaligashanmugam@gmail.com>
2026-03-25 13:38:37 +0530

    Fixed unsafe feof() usage in textFile()

--- trans.c
+++ trans.c
@@ -81,7 +81,7 @@ void textFile(FILE *readPtr)
         fprintf(writePtr, "%-6s%-16s%-11s%10s\n", "Acct", "Last Name", "First Name", "Balance");
 
         // copy all records from random-access file into text file
-        while (!feof(readPtr))
+        while (fread(&client, sizeof(struct clientData), 1, readPtr) == 1)
         {
             result = fread(&client, sizeof(struct clientData), 1, readPtr);
 
